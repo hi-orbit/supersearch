20309731bdf7aa943883dbe762d7378ec9195b6f
Rob Locke <rob@kodal.uk>
2024-02-12 10:55:21 +0000

    Versioned

--- supersearch.php
+++ supersearch.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: SuperSearch
  * Description: SuperSearch provides a hyperfast search solution for your WordPress site.
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: https://www.hi-orbit.com
  */
 
